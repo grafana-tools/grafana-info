5631b009c1a5f39c82d73d870010c67fdc319161
Alexander I.Grafov <grafov@gmail.com>
2016-11-22 03:55:25 +0300

    Set fix width for the columns.

--- template.go
+++ template.go
@@ -10,9 +10,9 @@ var (
 {{if .Tags}}Tags: {{range $k,$v := .Tags}}{{printf "[%s] " $v}}{{end}}
 {{end}}{{if .Templating.List}}Templating Vars: {{range $k,$v := .Templating.List}}{{printf "[%s] " $v.Name}}{{end}}
 {{end}}{{range $rowI,$rowV := .Rows}}{{printf "--- Row %s ---" $rowV.Title}}
-  {{range $panelI,$panelV := $rowV.Panels}}{{printf "| %.24s " $panelV.Title}}|{{end}}
+  {{if $rowV.Panels}}|{{end}}{{range $panelI,$panelV := $rowV.Panels}}{{printf " %-20.20s " $panelV.Title}}|{{end}}
+  {{if $rowV.Panels}}|{{end}}{{range $panelI,$panelV := $rowV.Panels}}{{printf " %-20.20s " $panelV.Type}}|{{end}}
 {{end}}
-
 `))
 	datasourceDisplay = template.Must(template.New("ds").Parse(
 		`=== Datasource "{{.Name}} ==="
