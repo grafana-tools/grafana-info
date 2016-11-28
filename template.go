5abdd38b7f8946e124c8fdd2e26161bafcecd39f
Alexander I.Grafov <grafov@gmail.com>
2016-11-29 02:55:58 +0300

    Add annotations output to the screen.

--- template.go
+++ template.go
@@ -27,9 +27,10 @@ import (
 
 var (
 	dashboardDisplay = template.Must(template.New("db").Parse(
-		`=== Dashboard <{{.ID}}> "{{.Title}}" ===
+		`=== Dashboard <{{.ID}}> "{{.Title}}" by [author] ===
 {{if .Tags}}Tags: {{range $k,$v := .Tags}}{{printf "[%s] " $v}}{{end}}
 {{end}}{{if .Templating.List}}Templating Vars: {{range $k,$v := .Templating.List}}{{printf "[%s] " $v.Name}}{{end}}
+{{end}}{{if .Annotations.List}}Annotations: {{range $k,$v := .Annotations.List}}{{printf "[%s] " $v.Name}}{{end}}
 {{end}}{{range $rowI,$rowV := .Rows}}{{printf "--- Row %s ---" $rowV.Title}}
   {{if $rowV.Panels}}|{{end}}{{range $panelI,$panelV := $rowV.Panels}}{{printf " %-20.20s " $panelV.Title}}|{{end}}
   {{if $rowV.Panels}}|{{end}}{{range $panelI,$panelV := $rowV.Panels}}{{printf " %-20.20s " $panelV.Type}}|{{end}}
